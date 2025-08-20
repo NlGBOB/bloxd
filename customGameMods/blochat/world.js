d6ac19a567d963a866e281227ecde59fded09045
NlGBOB <nlgbobmail@gmail.com>
2025-08-20 18:22:26 +0300

    slightly change structure of init

--- customGameMods/blochat/world.js
+++ customGameMods/blochat/world.js
@@ -1,30 +1,75 @@
-const modules = ["blochat", "foo", "bar"];
+const modules = [
+    {
+        chestPos: [0, 0, 0],
+        moduleMap: {
+            blochat: [1, 2, 3],
+            foo: [4, 5],
+            bar: [6]
+        }
+    },
+    {
+        chestPos: [1, 0, 0],
+        moduleMap: {
+            blochat: [7, 8],
+            baz: [9]
+        }
+    },
+    {
+        chestPos: [0, 1, 0],
+        moduleMap: {
+            qux: [10, 11],
+            foo: [12]
+        }
+    }
+];
+
+
 this.game = this.game || new (class {
-    constructor(moduleKeys = []) {
-        moduleKeys.forEach(key => {
-            this[key] = { initialized: false };
-        });
-    }
-    setInitialized(moduleKey) {
-        if (this[moduleKey]) this[moduleKey].initialized = true;
-    }
-    allModulesInitialized() {
-        return modules.every(key => this[key]?.initialized === true);
+    constructor(modulesMap = {}) {
+        this.moduleNames = []
+        for (const item of modulesMap) {
+            const { chestPos, moduleMap } = item;
+            for (const [moduleName, chestIdx] of Object.entries(moduleMap)) {
+                this.moduleNames.push(moduleName);
+                this[moduleName] = {};
+                this[moduleName].initialized = false;
+                this[moduleName].chestPos = chestPos;
+                this[moduleName].chestIdx = chestIdx;
+            }
+        }
+        this.initializeModules();
     }
 
-    // doesn't work gotta find smth smarter
     initializeModules() {
-        Object.keys(moduleInitializers).forEach(key => {
-            if (this[key] && !this[key].initialized) {
-                moduleInitializers[key](this); // pass the game singleton
-            }
-        });
+        for (const moduleName of this.moduleNames) {
+            const module = this[moduleName];
+
+            // if (no errors)
+            this.setInitialized(moduleName)
+        }
     }
+
+
+    setInitialized(moduleName) {
+        this[moduleName].initialized = true;
+    }
+
+    allModulesInitialized() {
+        return Object.keys(this._modulesData).every(
+            key => this[key]?.initialized === true
+        );
+    }
+
+
 })(modules);
 
 
+
+
+
 tick = () => {
-    if (!this.game.allModulesInitialized()) {
+    if (!this.game.pendingInit && !this.game.allModulesInitialized()) {
+        this.game.initializeModules();
         return;
     }
     tick_blochat();
